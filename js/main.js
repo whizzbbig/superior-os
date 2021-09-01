67142465804ccf0e7c513984194e60fe5fe96655
whizzbbig <54703305+whizzbbig@users.noreply.github.com>
2021-09-01 19:19:04 +0530

    dsfds

--- js/main.js
+++ js/main.js
@@ -162,75 +162,75 @@ slider.addEventListener("mousemove", (e) => {
 //   });
 // };
 
-// PRELOADER
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById("bm"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./data.json",
-});
+// // PRELOADER
+// var animation = bodymovin.loadAnimation({
+//   container: document.getElementById("bm"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "./data.json",
+// });
 
-const images = document.querySelectorAll("img");
-let isLoaded = false;
-let isLoadingAnimationEnd = false;
-const imgLoad = imagesLoaded(images);
+// const images = document.querySelectorAll("img");
+// let isLoaded = false;
+// let isLoadingAnimationEnd = false;
+// const imgLoad = imagesLoaded(images);
 
-const entranceAnimation = () => {
-  const tl = gsap.timeline();
-  tl.to("#bm", {
-    y: -100,
-    duration: 1,
-    ease: "power2.inOut",
-  })
-    .to(
-      ".loading",
-      {
-        yPercent: -100,
-        duration: 1.25,
-        ease: "power4.inOut",
-      },
-      0
-    )
-    .to(
-      ".content",
-      {
-        duration: 1,
-        opacity: 1,
-        y: 0,
-        stagger: 0.1,
-        ease: "power2.out",
-      },
-      0.6
-    );
-};
+// const entranceAnimation = () => {
+//   const tl = gsap.timeline();
+//   tl.to("#bm", {
+//     y: -100,
+//     duration: 1,
+//     ease: "power2.inOut",
+//   })
+//     .to(
+//       ".loading",
+//       {
+//         yPercent: -100,
+//         duration: 1.25,
+//         ease: "power4.inOut",
+//       },
+//       0
+//     )
+//     .to(
+//       ".content",
+//       {
+//         duration: 1,
+//         opacity: 1,
+//         y: 0,
+//         stagger: 0.1,
+//         ease: "power2.out",
+//       },
+//       0.6
+//     );
+// };
 
-const loadingAnimation = () => {
-  const tl = gsap
-    .timeline({
-      onComplete: () => {
-        isLoadingAnimationEnd = true;
-        if (isLoaded) entranceAnimation();
-      },
-    })
-    .from(".loading", {
-      yPercent: 100,
-      ease: "power3.inOut",
-      duration: 1,
-    })
-    .from(
-      "#bm",
-      {
-        y: 80,
-        duration: 1,
-        ease: "power2.out",
-      },
-      0.5
-    );
-};
+// const loadingAnimation = () => {
+//   const tl = gsap
+//     .timeline({
+//       onComplete: () => {
+//         isLoadingAnimationEnd = true;
+//         if (isLoaded) entranceAnimation();
+//       },
+//     })
+//     .from(".loading", {
+//       yPercent: 100,
+//       ease: "power3.inOut",
+//       duration: 1,
+//     })
+//     .from(
+//       "#bm",
+//       {
+//         y: 80,
+//         duration: 1,
+//         ease: "power2.out",
+//       },
+//       0.5
+//     );
+// };
 
-loadingAnimation();
-imgLoad.on("always", function () {
-  isLoaded = true;
-  if (isLoadingAnimationEnd) entranceAnimation();
-});
+// loadingAnimation();
+// imgLoad.on("always", function () {
+//   isLoaded = true;
+//   if (isLoadingAnimationEnd) entranceAnimation();
+// });
