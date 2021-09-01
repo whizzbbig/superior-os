c35abe0325a7ae0377a33cdd0cf9b9e81257ff2c
whizzbbig <54703305+whizzbbig@users.noreply.github.com>
2021-09-01 19:10:59 +0530

    removed canvas :(

--- js/main.js
+++ js/main.js
@@ -9,7 +9,18 @@ window.addEventListener("load", function () {
     inertia: 0.8,
     smooth: true,
     getDirection: true,
+    mobile: {
+      breakpoint: 0,
+      smooth: false,
+      getDirection: true,
+    },
+    tablet: {
+      breakpoint: 0,
+      smooth: false,
+      getDirection: true,
+    },
   });
+  
 
   scroller.on("scroll", function (t) {
     document.documentElement.setAttribute("data-direction", t.direction);
