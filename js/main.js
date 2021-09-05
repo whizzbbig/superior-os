515a413bdc02eb953d0460e3bd48e80f4a5ddec6
whizzbbig <54703305+whizzbbig@users.noreply.github.com>
2021-09-05 22:56:13 +0530

    did some changements with the locomotive scroll

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const entranceAnimation = () => {
       0
     )
     .to(
-      ".container",
+      "#scroller",
       {
         duration: 1,
         opacity: 1,
@@ -53,6 +53,7 @@ const loadingAnimation = () => {
     );
 };
 
+// Desktop View
 const phoneMockup = () => {
   // Adding scene to a app
   const app = new SpeRuntime.Application();
@@ -70,7 +71,8 @@ const phoneMockup = () => {
 loadingAnimation();
 imgLoad.on("always", function () {
   isLoaded = true;
-  if (isLoadingAnimationEnd)  entranceAnimation(); phoneMockup()
+  if (isLoadingAnimationEnd) entranceAnimation();
+  phoneMockup();
 
   // PRELOADER
   var animation = bodymovin.loadAnimation({
@@ -81,105 +83,174 @@ imgLoad.on("always", function () {
     path: "./data.json",
   });
 
-  // Desktop View
+  const scroller = document.querySelector("#scroller");
 
-  const pageContainer = document.querySelector(".container");
-  pageContainer.setAttribute("data-scroll-container", "");
-
-  const scroller = new LocomotiveScroll({
-    el: pageContainer,
-    inertia: 0.8,
+  const locoScroll = new LocomotiveScroll({
+    el: scroller,
     smooth: true,
-    getDirection: true,
-    mobile: {
-      smooth: false,
-      getDirection: true,
-      inertia: 0,
-    },
-    tablet: {
-      breakpoint: 0,
-      smooth: false,
-      getDirection: true,
-    },
   });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update());
 
-  scroller.on("scroll", function (t) {
-    document.documentElement.setAttribute("data-direction", t.direction);
-  });
-
-  scroller.on("scroll", ScrollTrigger.update);
-
-  ScrollTrigger.scrollerProxy(pageContainer, {
+  // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(scroller, {
     scrollTop(value) {
       return arguments.length
-        ? scroller.scrollTo(value, 0, 0)
-        : scroller.scroll.instance.scroll.y;
-    },
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
     getBoundingClientRect() {
       return {
-        left: 0,
         top: 0,
+        left: 0,
         width: window.innerWidth,
         height: window.innerHeight,
       };
     },
-    pinType: pageContainer.style.transform ? "transform" : "fixed",
+
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: scroller.style.transform ? "transform" : "fixed",
   });
 
-  // Pinning and horizontal scrolling
+  ScrollTrigger.defaults({
+    scroller: scroller,
+  });
 
-  let horizontalSections = document.querySelectorAll(".horizontal-scroll");
+  const horizontalSections = gsap.utils.toArray("section.horizontal");
 
-  horizontalSections.forEach((horizontalSection) => {
-    let pinWrap = horizontalSection.querySelector(".pin-wrap");
-    let pinWrapWidth = pinWrap.offsetWidth;
-    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-    gsap.to(pinWrap, {
-      scrollTrigger: {
-        scroller: "[data-scroll-container]",
-        scrub: true,
-        trigger: horizontalSection,
-        pin: true,
-        start: "top top",
-        end: () => `+=${pinWrapWidth}`,
-        invalidateOnRefresh: true,
+  horizontalSections.forEach(function (sec, i) {
+    let thisPinWrap = sec.querySelector(".pin-wrap");
+    let thisAnimWrap = thisPinWrap.querySelector(".animation-wrap");
+
+    let getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
+
+    gsap.fromTo(
+      thisAnimWrap,
+      {
+        x: () =>
+          thisAnimWrap.classList.contains(".to-right") ? 0 : getToValue(),
       },
-
-      x: -horizontalScrollLength,
-      ease: "none",
-    });
+      {
+        x: () =>
+          thisAnimWrap.classList.contains(".to-right") ? getToValue() : 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: sec,
+          scroller: scroller,
+          start: "top top",
+          end: () => "+=" + thisAnimWrap.scrollWidth,
+          pin: thisPinWrap,
+          invalidateOnRefresh: true,
+          anticipatePin: 1,
+          scrub: true,
+        },
+      }
+    );
   });
 
-  /* COLOR CHANGER */
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-  const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
-  scrollColorElems.forEach((colorSection, i) => {
-    const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
-    const prevText = i === 0 ? "" : scrollColorElems[i - 1].dataset.textcolor;
-
-    ScrollTrigger.create({
-      trigger: colorSection,
-      scroller: "[data-scroll-container]",
-      start: "top 50%",
-      onEnter: () =>
-        gsap.to("body", {
-          backgroundColor: colorSection.dataset.bgcolor,
-          color: colorSection.dataset.textcolor,
-          overwrite: "auto",
-        }),
-
-      onLeaveBack: () =>
-        gsap.to("body", {
-          backgroundColor: prevBg,
-          color: prevText,
-          overwrite: "auto",
-        }),
-    });
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => scroller.update());
-
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
+
+  // const pageContainer = document.querySelector(".container");
+  // pageContainer.setAttribute("data-scroll-container", "");
+
+  // const scroller = new LocomotiveScroll({
+  //   el: pageContainer,
+  //   inertia: 0.8,
+  //   smooth: true,
+  //   getDirection: true,
+  //   mobile: {
+  //     smooth: false,
+  //     getDirection: true,
+  //     inertia: 0,
+  //   },
+  //   tablet: {
+  //     breakpoint: 0,
+  //     smooth: false,
+  //     getDirection: true,
+  //   },
+  // });
+
+  // scroller.on("scroll", function (t) {
+  //   document.documentElement.setAttribute("data-direction", t.direction);
+  // });
+
+  // scroller.on("scroll", ScrollTrigger.update);
+
+  // ScrollTrigger.scrollerProxy(pageContainer, {
+  //   scrollTop(value) {
+  //     return arguments.length
+  //       ? scroller.scrollTo(value, 0, 0)
+  //       : scroller.scroll.instance.scroll.y;
+  //   },
+  //   getBoundingClientRect() {
+  //     return {
+  //       left: 0,
+  //       top: 0,
+  //       width: window.innerWidth,
+  //       height: window.innerHeight,
+  //     };
+  //   },
+  //   pinType: pageContainer.style.transform ? "transform" : "fixed",
+  // });
+
+  // // Pinning and horizontal scrolling
+
+  // let horizontalSections = document.querySelectorAll(".horizontal-scroll");
+
+  // horizontalSections.forEach((horizontalSection) => {
+  //   let pinWrap = horizontalSection.querySelector(".pin-wrap");
+  //   let pinWrapWidth = pinWrap.offsetWidth;
+  //   let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+  //   gsap.to(pinWrap, {
+  //     scrollTrigger: {
+  //       scroller: "[data-scroll-container]",
+  //       scrub: true,
+  //       trigger: horizontalSection,
+  //       pin: true,
+  //       start: "top top",
+  //       end: () => `+=${pinWrapWidth}`,
+  //       invalidateOnRefresh: true,
+  //     },
+
+  //     x: -horizontalScrollLength,
+  //     ease: "none",
+  //   });
+  // });
+
+  // /* COLOR CHANGER */
+
+  // const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
+  // scrollColorElems.forEach((colorSection, i) => {
+  //   const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
+  //   const prevText = i === 0 ? "" : scrollColorElems[i - 1].dataset.textcolor;
+
+  //   ScrollTrigger.create({
+  //     trigger: colorSection,
+  //     scroller: "[data-scroll-container]",
+  //     start: "top 50%",
+  //     onEnter: () =>
+  //       gsap.to("body", {
+  //         backgroundColor: colorSection.dataset.bgcolor,
+  //         color: colorSection.dataset.textcolor,
+  //         overwrite: "auto",
+  //       }),
+
+  //     onLeaveBack: () =>
+  //       gsap.to("body", {
+  //         backgroundColor: prevBg,
+  //         color: prevText,
+  //         overwrite: "auto",
+  //       }),
+  //   });
+  // });
+
+  // ScrollTrigger.addEventListener("refresh", () => scroller.update());
+
+  // ScrollTrigger.refresh();
 });
 
 // SLIDER FOR SCREENSHOT SECTION ON MOBILE
